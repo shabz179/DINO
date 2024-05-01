a4f5f422a892a86018470e50a71657f1f31ba5e7
Juhaer26 <168012968+Juhaer26@users.noreply.github.com>
2024-05-01 10:41:28 +0100

    Delete juhaerapp directory

--- juhaerapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Equipment
-from .models import Bookinglog
-
-# Register your models here.
-
-admin.site.register(Equipment)
-admin.site.register(Bookinglog)
